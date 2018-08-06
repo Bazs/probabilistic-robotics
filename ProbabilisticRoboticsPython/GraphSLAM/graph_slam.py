5a630b7b4f49464eacbc4e0ab9dc357cce8e1ed8
Istvan Balazs Opra <opra.balazs@gmail.com>
2018-08-06 08:59:02 +0200

    Fixed accumulating overly large velocity and turn rate when facing obstacles

--- ProbabilisticRoboticsPython/GraphSLAM/graph_slam.py
+++ ProbabilisticRoboticsPython/GraphSLAM/graph_slam.py
@@ -44,27 +44,31 @@ for step in range(ROBOT_STEP_COUNT):
     proposal_state_is_valid = False
 
     while proposal_state_is_valid is False:
-        v = v + rnd.normalvariate(0, VELOCITY_VARIANCE)
-        v = clip(v, 0, MAX_VELOCITY)
+        v_proposal = v + rnd.normalvariate(0, VELOCITY_VARIANCE)
+        v_proposal = clip(v_proposal, 0, MAX_VELOCITY)
 
-        omega = omega + rnd.normalvariate(0, TURN_RATE_VARIANCE)
-        omega = clip(omega, -MAX_TURN_RATE, MAX_TURN_RATE)
+        omega_proposal = omega + rnd.normalvariate(0, TURN_RATE_VARIANCE)
+        omega_proposal = clip(omega_proposal, -MAX_TURN_RATE, MAX_TURN_RATE)
 
         # CTRV moditon model
-        delta_pos = np.array([[v / omega * (sin(omega + current_state[2]) - sin(current_state[2])),
-                               v / omega * (-cos(omega + current_state[2]) + cos(current_state[2])),
-                               omega]]).T
+        delta_pos = np.array([[v_proposal/omega_proposal * (sin(omega_proposal + current_state[2])
+                                                            - sin(current_state[2])),
+                               v_proposal/omega_proposal * (-cos(omega_proposal + current_state[2])
+                                                            + cos(current_state[2])),
+                               omega_proposal]]).T
         proposal_state = current_state + delta_pos
 
-        # TODO: fix the validity check: obstacles go from x.5 to x.5+1 !
-        proposal_x = int(proposal_state[0])
-        proposal_y = int(proposal_state[1])
+        proposal_x = round(proposal_state.item(0))
+        proposal_y = round(proposal_state.item(1))
 
         proposal_state_is_within_map = 0 <= proposal_y < MAP_WIDTH and 0 <= proposal_x < MAP_HEIGHT
+        # TODO: fix the validity check: obstacles go from x.5 to x.5+1 ! Still doesn't work...
+        proposal_state_is_valid = proposal_state_is_within_map and (0 == ground_truth_map[proposal_y, proposal_x])
 
-        proposal_state_is_valid = proposal_state_is_within_map and 0 == ground_truth_map[proposal_y, proposal_x]
+    v = v_proposal
+    omega = omega_proposal
+    print(ground_truth_map[proposal_y, proposal_x])
 
-    # TODO: investigate too-abrupt turns
     proposal_theta = proposal_state[2]
 
     while -math.pi > proposal_theta:
@@ -86,6 +90,9 @@ path_y = []
 for state in ground_truth_states:
     path_x.append(state[0])
     path_y.append(state[1])
+
+print(path_x)
+print(path_y)
 
 plt.plot(path_x, path_y, marker='o')
 
