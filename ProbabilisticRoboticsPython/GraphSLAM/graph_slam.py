53cca0f97fb75d35db247b1522630bf5584a65f9
Istvan Balazs Opra <opra.balazs@gmail.com>
2018-08-10 08:00:41 +0200

    Started adding stochastic "sensor model", for generating landmark detections along the ground truth path

--- ProbabilisticRoboticsPython/GraphSLAM/graph_slam.py
+++ ProbabilisticRoboticsPython/GraphSLAM/graph_slam.py
@@ -2,6 +2,7 @@ from slam_utils.map import generate_ground_truth_map
 from slam_utils.path_generator import generate_ground_truth_path
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 import math
 import random as rnd
@@ -10,6 +11,7 @@ MAP_WIDTH = 50
 MAP_HEIGHT = 50
 OBSTACLE_COUNT = 20
 
+# TODO rename obstacles to landmarks
 ground_truth_map, obstacles = generate_ground_truth_map(MAP_HEIGHT, MAP_WIDTH, OBSTACLE_COUNT)
 
 # Set up truly random number generation for creating the ground truth path (if the system supports it)
@@ -26,8 +28,24 @@ ground_truth_states = generate_ground_truth_path(ground_truth_map, max_velocity=
                                                  velocity_variance=velocity_variance, max_turn_rate=max_turn_rate,
                                                  turn_rate_variance=turn_rate_variance, step_count=step_count)
 
-MAX_SENSING_RANGE = 10
-SENSING_RANGE_VARIANCE = 6
+MAX_SENSING_RANGE = 15
+SENSING_RANGE_VARIANCE = 13
+
+for ground_truth_state in ground_truth_states:
+    # Select obstacles and their distances to current state, if the distance  is lower than the threshold
+    obstacle_distances = [np.linalg.norm(obstacle[:2] - ground_truth_state[:2]) for obstacle in obstacles]
+    obstacles_in_range, in_range_distances = zip(*[(obstacle, obstacle_distances[index]) for index, obstacle in
+                                                   enumerate(obstacles) if obstacle_distances[index]
+                                                   <= MAX_SENSING_RANGE])
+
+    # Sample obstacles from the in-range ones, using the absolute values of samples from a zero-mean normal
+    # distribution as distance thresholds
+    detected_obstacles, detected_distances = zip(*[(obstacle, in_range_distances[index]) for index, obstacle in
+                                                   enumerate(obstacles_in_range) if
+                                                   abs(rnd.normalvariate(0, SENSING_RANGE_VARIANCE))
+                                                   >= in_range_distances[index]])
+
+    # TODO calculate measurement vector (r, phi, s) for detected obstacles
 
 plt.plot()
 plt.title("Ground truth map")
