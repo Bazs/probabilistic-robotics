6228620e21612226e5c61cf2e42ccfa40d5c1088
Istvan Balazs Opra <opra.balazs@gmail.com>
2018-08-15 14:30:08 +0200

    Started with the implementation of GraphSLAM linearize. Information from controls is now aggregated into the information matrix.

--- ProbabilisticRoboticsPython/GraphSLAM/graph_slam.py
+++ ProbabilisticRoboticsPython/GraphSLAM/graph_slam.py
@@ -5,6 +5,8 @@ from slam_utils.measurement_model import generate_measurements
 from slam_utils.path_generator import generate_ground_truth_path
 from slam_utils.plot_utils import plot_path, plot_measurements_for_state
 
+from slam_utils.ctrv_motion_model import calculate_odometry_from_controls, calculate_jacobian_from_controls
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -44,14 +46,42 @@ if __name__ == "__main__":
                                          sensing_range_deviation=SENSING_RANGE_DEVIATION,
                                          distance_deviation=DISTANCE_DEVIATION, heading_deviation=HEADING_DEVIATION)
 
-    initial_state_estimates = graph_slam_initialize(controls, state_t0=ground_truth_states[0])
+    state_estimates = graph_slam_initialize(controls, state_t0=ground_truth_states[0])
+
+    # Perform GraphSLAM linearize
+    omega = np.identity(3, dtype="float") * 100000
+    xi = np.empty((1, 1))
+
+    R = np.identity(3)
+
+    for index, control in enumerate(controls[1:]):
+        v = control.item(0)
+        om = control.item(1)
+        previous_state = state_estimates[index]
+        current_state_estimate = state_estimates[index] + calculate_odometry_from_controls(v, om, previous_state)
+
+        # Calculate the 6-by-6 update for the information matrix Omega
+        Gt = np.identity(3)
+        Gt[:, -1] = np.ravel(calculate_jacobian_from_controls(v, om, previous_state))
+        omega_xt_xt_1 = np.dot(np.concatenate((-Gt.T, np.identity(3))), np.linalg.inv(R))
+        omega_xt_xt_1 = np.dot(omega_xt_xt_1, np.concatenate((-Gt, np.identity(3)), axis=1))
+
+        upper_left_index = index * 3
+        lower_right_index = index * 3 + 6
+
+        # Expand Omega to accommodate the new information from the motion
+        while lower_right_index > omega.shape[0]:
+            omega = np.concatenate((omega, np.zeros((omega.shape[0], 1))), axis=1)
+            omega = np.concatenate((omega, np.zeros((1, omega.shape[1]))), axis=0)
+
+        omega[upper_left_index:lower_right_index, upper_left_index:lower_right_index] += omega_xt_xt_1
 
     plt.plot()
     plt.title("Ground truth map")
     plt.imshow(ground_truth_map, origin='lower')
 
     plot_path(ground_truth_states, 'C0')
-    plot_path(initial_state_estimates, 'C1')
+    plot_path(state_estimates, 'C1')
 
     current_state = 1
     plot_measurements_for_state(ground_truth_states[current_state], measurements[current_state])
--- ProbabilisticRoboticsPython/GraphSLAM/slam_utils/ctrv_motion_model.py
+++ ProbabilisticRoboticsPython/GraphSLAM/slam_utils/ctrv_motion_model.py
@@ -9,3 +9,9 @@ def calculate_odometry_from_controls(v, omega, previous_state):
     return np.array([[v / omega * (sin(omega + previous_state[2]) - sin(previous_state[2])),
                     v / omega * (-cos(omega + previous_state[2]) + cos(previous_state[2])),
                     omega]]).T
+
+
+def calculate_jacobian_from_controls(v, omega, previous_state):
+    return np.array([[v / omega * (cos(omega + previous_state[2]) - cos(previous_state[2])),
+                     v / omega * (sin(omega + previous_state[2] - sin(previous_state[2]))),
+                     1]]).T
