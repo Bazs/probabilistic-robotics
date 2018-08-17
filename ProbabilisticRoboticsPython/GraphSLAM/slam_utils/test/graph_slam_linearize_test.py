55d26849dae2f79b0da9b110ae2324869fcc92fe
Istvan Balazs Opra <opra.balazs@gmail.com>
2018-08-17 20:50:16 +0200

    Extended the linearize_controls test to include circular movement

--- ProbabilisticRoboticsPython/GraphSLAM/slam_utils/test/graph_slam_linearize_test.py
+++ ProbabilisticRoboticsPython/GraphSLAM/slam_utils/test/graph_slam_linearize_test.py
@@ -1,5 +1,7 @@
 from slam_utils.graph_slam_linearize import linearize_controls
 
+from slam_utils.ctrv_motion_model import calculate_odometry_from_controls
+
 import numpy as np
 
 import unittest
@@ -8,39 +10,41 @@ import unittest
 class TestGraphSlamLinearize(unittest.TestCase):
 
     def test_linearize_controls_round_trip(self):
-        # Initial state is extremely reliable, i.e. high values in the information matrix
-        omega = np.identity(3, dtype="float") * 100000
-        xi = np.zeros((1, 1))
 
-        # Perfect controls, without noise
-        controls = [np.array([[1, 0]]).T] * 5
+        # Test a straight movement, and circular movement
+        test_controls = [
+            [np.array([[1, 0]]).T] * 5,
+            [np.array([[1, 0.1]]).T] * 5
+        ]
 
-        # Perfect estimates, according to controls
-        state_estimates = [
-            np.array([[0, 0, 0]]).T,
-            np.array([[1, 0, 0]]).T,
-            np.array([[2, 0, 0]]).T,
-            np.array([[3, 0, 0]]).T,
-            np.array([[4, 0, 0]]).T,
-            ]
+        for controls in test_controls:
+            # Initial state is extremely reliable, i.e. high values in the information matrix
+            omega = np.identity(3, dtype="float") * 100000
+            xi = np.zeros((1, 1))
 
-        # Negligible motion error covariance
-        R = np.identity(3) * 0.00001
+            # Perfect estimates, according to controls
+            state_estimates = [np.array([[0, 0, 0]]).T]
 
-        # Calculate the information matrix and vector from linearized controls
-        xi, omega = linearize_controls(xi, omega, R, controls, state_estimates)
+            for index, control in enumerate(controls[1:]):
+                state_estimates.append(state_estimates[index]
+                                       + calculate_odometry_from_controls(control.item(0), control.item(1),
+                                                                          state_estimates[index]))
 
-        # Recover the moments representation of belief, i.e. the covariance and mean
-        sigma = np.linalg.inv(omega)
-        mu = np.dot(sigma, xi)
+            # Negligible motion error covariance
+            R = np.identity(3) * 0.00001
 
-        # Assert that the recovered mean is indistinguishable from the input state estimates
-        for index, state_estimate in enumerate(state_estimates):
-            mu_base_index = index * 3
-            recovered_estimate = np.array([[mu[mu_base_index], mu[mu_base_index + 1], mu[mu_base_index + 2]]]).T
-            self.assertTrue(np.allclose(recovered_estimate, state_estimate))
+            # Calculate the information matrix and vector from linearized controls
+            xi, omega = linearize_controls(xi, omega, R, controls, state_estimates)
 
-        return
+            # Recover the moments representation of belief, i.e. the covariance and mean
+            sigma = np.linalg.inv(omega)
+            mu = np.dot(sigma, xi)
+
+            # Assert that the recovered mean is indistinguishable from the input state estimates
+            for index, state_estimate in enumerate(state_estimates):
+                mu_base_index = index * 3
+                recovered_estimate = np.array([[mu[mu_base_index], mu[mu_base_index + 1], mu[mu_base_index + 2]]]).T
+                self.assertTrue(np.allclose(recovered_estimate, state_estimate))
 
 
 if __name__ == "__main__":
